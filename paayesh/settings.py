4bec2d9eaf33bb01540abded9ce2a511f8c8bc83
Amir Hossein <amirsorouri26@gmail.com>
2019-01-06 12:42:43 +0330

    static directory from djangoSchool Project application migrated and static dir inside settings.py added.

--- paayesh/settings.py
+++ paayesh/settings.py
@@ -64,7 +64,8 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'templates')
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'static')
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -164,6 +165,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 
 
